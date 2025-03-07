f1b25e4beefe2b265ea753e63388872e4ecffef9
soniya-natarajan <soniyanatarajan12@gmail.com>
2025-03-07 14:01:12 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,6 +1,8 @@
 provider "aws" {
-  region = "us-east-1"
+  region  = "us-east-1"
+  profile = "terraform-user"
 }
+
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "my-s3-bucket"
   acl    = "private"
