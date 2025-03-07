ba088da053b064aa1fd8d695b371e11982af6c0a
soniya-natarajan <soniyanatarajan12@gmail.com>
2025-03-07 14:32:18 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -4,7 +4,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "my-s3-bucket"
+  bucket = "my-s3-bucket11"
   acl    = "private"
 tags = {
     Name        = "My S3 Bucket"
